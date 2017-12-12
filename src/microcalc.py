29eb07763bfe4ec6d302dafccac3977f5f6d9ffd
alberto castillo gordón <acastillogordon@gmail.com>
2017-12-12 10:08:44 -0500

    Refactor del programa principal

--- src/microcalc.py
+++ src/microcalc.py
@@ -5,61 +5,67 @@ from AST import *
 from sys import stdin
 from errores import *
 
+def main():
+    args = parseArgs()
+
+    while True:
+        linea = stdin.readline()
+
+        if linea == '\n':
+            break
+
+        if args.tokens:
+            print printTokens(linea)
+        elif args.expresiones:
+            print "Expresion: " + getExpresion(linea)
+            print "Resultado = " + str(getResultado(linea))
+        else:
+            print "Resultado = " + str(getResultado(linea))
+
+# Devuelve el resultado de la expresion
+def getResultado(linea):
+        try:
+            lexico = Lexico(linea)
+            sintactico = Sintactico(lexico)
+            expresion = sintactico.analizaLinea()
+
+            return expresion.evalua()
+        except Exception:
+            print "Error de Ejecucion. (Resultado)"
+
+# Devuelva la cadena que muestra la jerarquia de la expresion
+def getExpresion(linea):
+        try:
+            lexico = Lexico(linea)
+            sintactico = Sintactico(lexico)
+            expresion = sintactico.analizaLinea()
+
+            return str(expresion)
+        except Exception:
+            print "Error en Ejecucion. (Expresiones)"
+
+# Devuelve los tokens separados por saltos de linea
+def printTokens(linea):
+    lexico = Lexico(linea)
+    tokens = ''
+
+    while True:
+        try:
+            componente = lexico.siguiente()
+            tokens = tokens + '\n' + str(componente)
+
+            if componente.cat == 'eof':
+                break
+        except Exception:
+            print "Error de ejecucion. (Tokens)"
+
+    return tokens
+
 def parseArgs():
-    parser = argparse.ArgumentParser(description='Calculadora con funciones basicas, mediante el la estructura de arbol con gramatica limitada.');
+    parser = argparse.ArgumentParser(description='Calculadora con funciones basicas, mediante una estructura de arbol con gramatica limitada.');
     parser.add_argument('-t', '--tokens', action='store_true', help='Muestra los tokens ingresados en cada linea.', required=False);
     parser.add_argument('-e', '--expresiones', action='store_true', help='Muestra como se separan las expresiones de la linea.', required=False);
     return parser.parse_args();
 
 if __name__ == '__main__':
-    args = parseArgs()
-
-    if args.tokens == True:
-        while True:
-            linea = stdin.readline()
-
-            if linea == '\n':
-                break
-
-            lexico = Lexico(linea)
-
-            while True:
-                try:
-                    componente = lexico.siguiente()
-                    print componente
-
-                    if componente.cat == 'eof':
-                        break
-                except Exception:
-                    print "Error de ejecucion. (Tokens)"
-
-    elif args.expresiones == True:
-        while True:
-            linea = stdin.readline()
-
-            if linea == '\n':
-                break
-
-            try:
-                lexico = Lexico(linea)
-                sintactico = Sintactico(lexico)
-
-                expresion = sintactico.analizaLinea()
-                print "La expresion es : " + str(expresion) + " = " + str(expresion.evalua())
-            except Exception:
-                print "Error en Ejecucion. (Expresiones)"
-    else:
-        while True:
-            linea = stdin.readline()
-
-            if linea == '\n':
-                break
-
-            try:
-                lexico = Lexico(linea)
-                sintactico = Sintactico(lexico)
-
-                expresion = sintactico.analizaLinea()
-                print "Resultado: " + str(expresion.evalua())
-            except Exception:
-                print "Error de Ejecucion"
+    main()
